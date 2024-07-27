f32261016805cb21f9e236ed48bb2fa5a77558b9
GoldFoxYT <goldfoxyt1@outlook.com>
2024-07-28 01:10:36 +0300

    made the BANANA less powerful

--- src/main/java/net/goldfoxyt/moremilk/item/ModFoodProperties.java
+++ src/main/java/net/goldfoxyt/moremilk/item/ModFoodProperties.java
@@ -3,5 +3,5 @@ package net.goldfoxyt.moremilk.item;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
-    public static final FoodProperties BANANA = new FoodProperties.Builder().nutrition(4).saturationModifier(2.4f).build();
+    public static final FoodProperties BANANA = new FoodProperties.Builder().nutrition(5).saturationModifier(0.5F).build();
 }
